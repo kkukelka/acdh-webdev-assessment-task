7391ead499b554168923f23fbd9baa17343e9b74
kkukelka <17953978+kkukelka@users.noreply.github.com>
2024-02-25 19:41:13 +0100

    feat: add searchfield component

---
+++ src/components/SearchField.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { useVModel } from "@vueuse/core";
+
+const props = defineProps<{
+  modelValue?: string | number;
+}>();
+
+const emits = defineEmits<{
+  (e: "update:modelValue", payload: string | number): void;
+}>();
+
+const modelValue = useVModel(props, "modelValue", emits, {
+  passive: true,
+});
+</script>
+
+<template>
+  <div class="relative w-full max-w-sm items-center">
+    <input
+      v-model="modelValue"
+      id="search"
+      type="text"
+      placeholder="Search..."
+      class="pl-10 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+    />
+    <span
+      class="text-stone-100 absolute start-0 inset-y-0 flex items-center justify-center px-2"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-5"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+        />
+      </svg>
+    </span>
+  </div>
+</template>
